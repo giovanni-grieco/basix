4827ca72258ff0beda34dca282f2157f8595e4da
Giovanni Pio Grieco <giovi.5057@gmail.com>
2025-04-24 22:29:17 +0200

    fix

--- show_time.lua
+++ show_time.lua
@@ -1,8 +1,14 @@
+local first_run = true
 while true do
     -- Get the current time
     local current_time = os.date("%H:%M:%S")
     -- Clear the screen
-    os.execute("clear") -- Use "cls" for Windows
+    if first_run then
+        -- Clear the screen only on the first run
+        first_run = false
+    else 
+        print("\27[2J\27[H")
+    end
     -- Print the current time
     print("Current Time: " .. current_time)
     -- Wait for 1 second before updating the time
