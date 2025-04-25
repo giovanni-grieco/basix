79cc07d8805e8c26a435731813ae65742c36ccf5
Giovanni Grieco <giovi.5057@gmail.com>
2025-04-25 14:24:40 +0100

    fix

--- install.lua
+++ install.lua
@@ -6,24 +6,27 @@ print("Downloading file list from"  .. file_list_url)
 
 local file_list = http.get(file_list_url).readAll()
 
-print(file_list)
+local file_list_lines = {}
 
-local file_list_lines = textutils.unserialize(file_list)
+for line in file_list:gmatch("[^\r\n]+") do
+  table.insert(file_list_lines, line)
+end
 
-for _, file in ipairs(file_list_lines) do
-    local file_url = url_prefix .. file
-    local file_path = shell.resolve(file)
-
-    print("Downloading" .. file_url ..  "to"  .. file_path)
-
-    local response = http.get(file_url)
-    if response then
-        local file_data = response.readAll()
-        local file_handle = fs.open(file_path, w)
-        file_handle.write(file_data)
-        file_handle.close()
-        print("Downloaded and saved"  .. file_path)
+for i, line in ipairs(file_list_lines) do
+    local file_url = url_prefix .. line
+    local file_name = line:match("([^/]+)$")
+    
+    print("Downloading " .. file_name .. " from " .. file_url)
+    
+    local file_data = http.get(file_url).readAll()
+    
+    if file_data then
+        local file = fs.open(file_name, "w")
+        file.write(file_data)
+        file.close()
+        print("Downloaded and saved " .. file_name)
     else
-        print("Failed to download"  .. file_url)
+        print("Failed to download " .. file_name)
     end
 end
+
