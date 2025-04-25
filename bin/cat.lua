a6baeff7ea8fd46fbbde52c6d119487c7416366a
Giovanni Grieco <giovi.5057@gmail.com>
2025-04-25 15:12:25 +0100

    cat program

--- bin/cat.lua
+++ bin/cat.lua
@@ -1,1 +1,29 @@
-print("TBD")
+--[[ 
+    This script is a simple implementation of the Unix `cat` command in Lua for ComputerCraft.
+    It reads a file and prints its content to the console.
+    
+    Usage:
+        cat <file>
+    
+    Example:
+        cat myfile.txt
+]]--
+
+local file_path = arg[1]
+if not file_path then
+    print("Usage: cat <file>")
+    return
+end
+local file = fs.open(file_path, "r")
+if not file then
+    print("File not found: " .. file_path)
+    return
+end
+local content = file.readAll()
+file.close()
+
+if content then
+    print(content)
+else
+    print("Error reading file: " .. file_path)
+end
