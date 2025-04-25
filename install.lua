adecb4437f2515c2aaf7f9358a66327523613b58
Giovanni Grieco <59712930+giovanni-grieco@users.noreply.github.com>
2025-04-25 13:38:07 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,11 +1,29 @@
-local url_prefix = "https://raw.githubusercontent.com/giovanni-grieco/basix/refs/heads/main/"
+local url_prefix = httpsraw.githubusercontent.comgiovanni-griecobasixrefsheadsmain
 
-local file_list_url = url_prefix .. "filelist"
+local file_list_url = url_prefix .. filelist
 
-print("Downloading file list from: " .. file_list_url)
+print(Downloading file list from  .. file_list_url)
 
 local file_list = http.get(file_list_url).readAll()
 
 print(file_list)
 
 local file_list_lines = textutils.unserialize(file_list)
+
+for _, file in ipairs(file_list_lines) do
+    local file_url = url_prefix .. file
+    local file_path = shell.resolve(file)
+
+    print(Downloading  .. file_url ..  to  .. file_path)
+
+    local response = http.get(file_url)
+    if response then
+        local file_data = response.readAll()
+        local file_handle = fs.open(file_path, w)
+        file_handle.write(file_data)
+        file_handle.close()
+        print(Downloaded and saved  .. file_path)
+    else
+        print(Failed to download  .. file_url)
+    end
+end
