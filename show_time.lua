4b8c0b0e66a5cbaeb93c097c3ee9ae62824de3d0
Giovanni Grieco <59712930+giovanni-grieco@users.noreply.github.com>
2025-04-25 04:33:06 +0200

    Update show_time.lua

--- show_time.lua
+++ show_time.lua
@@ -1,12 +1,16 @@
 local first_run = true
 
 local monitor = peripheral.find("monitor")
+
 while true do
     -- Get the current time
-    local current_time = os.date("%H:%M:%S")
+    local current_time = os.time()
     -- Clear the screen
     monitor.clear()
     -- Write the current time to the monitor
+    monitor.setCursorPos(1,1)
+    monitor.write("In-Game time:")
+    monitor.setCursorPos(1,2)
     monitor.write(current_time)
     -- Wait for 1 second before updating the time
     os.sleep(1)
