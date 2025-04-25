ec780ca7b88446c7ad2cbb7feb70554037aa2a42
Giovanni Pio Grieco <giovi.5057@gmail.com>
2025-04-25 13:41:23 +0200

    fixed

--- install.lua
+++ install.lua
@@ -1,8 +1,8 @@
-local url_prefix = httpsraw.githubusercontent.comgiovanni-griecobasixrefsheadsmain
+local url_prefix = "https://raw.githubusercontent.comgiovanni-grieco/basix/refs/heads/main"
 
-local file_list_url = url_prefix .. filelist
+local file_list_url = url_prefix .. "filelist"
 
-print(Downloading file list from  .. file_list_url)
+print("Downloading file list from"  .. file_list_url)
 
 local file_list = http.get(file_list_url).readAll()
 
@@ -14,7 +14,7 @@ for _, file in ipairs(file_list_lines) do
     local file_url = url_prefix .. file
     local file_path = shell.resolve(file)
 
-    print(Downloading  .. file_url ..  to  .. file_path)
+    print("Downloading" .. file_url ..  "to"  .. file_path)
 
     local response = http.get(file_url)
     if response then
@@ -22,8 +22,8 @@ for _, file in ipairs(file_list_lines) do
         local file_handle = fs.open(file_path, w)
         file_handle.write(file_data)
         file_handle.close()
-        print(Downloaded and saved  .. file_path)
+        print("Downloaded and saved"  .. file_path)
     else
-        print(Failed to download  .. file_url)
+        print("Failed to download"  .. file_url)
     end
 end
