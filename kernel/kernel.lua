20e84cb696efdd799b09d9f0320b07ee1fc01cd9
giovanni <giovi.5057@gmail.com>
2025-04-28 15:11:04 +0200

    kernel dev

--- kernel/kernel.lua
+++ kernel/kernel.lua
@@ -17,16 +17,78 @@ local PROCESS_ZOMBIE = 5
 local kernel = {
     processes = {},
     next_pid = 1,
-    current_process = nil,
     ipc_queues = {},
     signals = {},
     sleeping_processes = {},
     default_environment = {}
 }
 
+-- Function to create a new process
+
+function kernel.create_process(name, entry_point, args, env)
+    local process = {
+        pid = kernel.next_pid,
+        name = name,
+        entry_point = entry_point, --name of the lua code to run
+        args = args,
+        env = env or kernel.default_environment,
+        state = PROCESS_NEW,
+        ipc_queue = {},
+        signal_handlers = {},
+        routine = coroutine.create(require(entry_point)),
+    }
+    kernel.processes[kernel.next_pid] = process
+    kernel.next_pid = kernel.next_pid + 1
+    return process
+end
+
+function kernel.destroy_process(pid)
+    local process = kernel.processes[pid]
+    if process then
+        process.state = PROCESS_TERMINATED
+        kernel.processes[pid] = nil
+    end
+end
+
+function kernel.schedule()
+    for _, process in pairs(kernel.processes) do
+        if process.state == PROCESS_NEW then
+            debug.sethook(process.routine,
+                function()
+                    print("kernel yield")
+                    coroutine.yield()
+                end,
+                'l',
+                1000
+        )
+            process.state = PROCESS_READY
+        end
 
 
+        if process.state == PROCESS_READY then
+            process.state = PROCESS_RUNNING
+            
+            -- Resume the coroutine
+            local success, result = coroutine.resume(process.routine, process.args)
+            print("kernel resume")
+            if not success then
+                print("Process error: " .. tostring(result))
+                process.state = PROCESS_TERMINATED
+            elseif coroutine.status(process.routine) == "dead" then
+                process.state = PROCESS_TERMINATED
+            else
+                -- Coroutine yielded and can be resumed later
+                process.state = PROCESS_READY
+            end
+        end
+    end
+end
 
-
+function kernel.run()
+    while true do
+        --kernel.schedule()
+        -- ADDITIONAL Stuff
+    end
+end
 
 return kernel
